ad74437f2843bacd54bebab7cb2b211037e64291
Liam Rutherford <liamrutherford10@gmail.com>
2024-10-02 17:32:58 +0100

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; // Arrays
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Sum of Array: {0}", SumOfArray(numbers));
+    }
+    static int SumOfArray(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+    }
